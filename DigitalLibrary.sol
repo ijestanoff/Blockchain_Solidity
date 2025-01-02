11bba44c504cc22962fdd41398e59bb03d3ba990
ijestanoff <jestanoff@icloud.com>
2025-01-02 22:33:05 +0200

    Digital Library GetBook

--- DigitalLibrary.sol
+++ DigitalLibrary.sol
@@ -18,15 +18,40 @@ struct EBook {
     uint256 readCount;
 }
 
-error YouAreNotAuthor();
+error You_Are_Not_Author();
+error You_Are_Not_Librarian();
 
 contract DigitalLibrary {
     uint256 private id;
     uint256 private expirationDays;
-    mapping(uint256 => EBook) public libraryBooks;
+    mapping(uint256 => EBook) private libraryBooks;
 
     constructor(uint256 _expirationDays) {
         expirationDays = _expirationDays;
+    }
+
+    function getLibraryBook(uint256 _bookId)
+        public
+        view
+        returns (
+            string memory title__,
+            string memory author__,
+            uint256 publicationDate__,
+            uint256 expirationDate__,
+            State status__,
+            address[] memory librarians__,
+            uint256 readCount__)
+    {
+        require(libraryBooks[_bookId].primaryLibrarian == msg.sender);
+        return (
+            libraryBooks[_bookId].title,
+            libraryBooks[_bookId].author,
+            libraryBooks[_bookId].publicationDate,
+            libraryBooks[_bookId].expirationDate,
+            libraryBooks[_bookId].status,
+            libraryBooks[_bookId].librarians,
+            libraryBooks[_bookId].readCount
+        );
     }
 
     function createEBook(string calldata _title, string calldata _author)
@@ -44,22 +69,41 @@ contract DigitalLibrary {
         id++;
     }
 
-    function addLibrarian(uint256 _id, address _librarian)
+    function addLibrarian(uint256 _bookId, address _librarian)
         public
-        returns (EBook memory book)
+        returns (address[] memory librarians)
     {
-        libraryBooks[_id].librarians.push(_librarian);
-        if (libraryBooks[_id].primaryLibrarian != msg.sender) {
-            revert YouAreNotAuthor();
+        libraryBooks[_bookId].librarians.push(_librarian);
+        if (libraryBooks[_bookId].primaryLibrarian != msg.sender) {
+            revert You_Are_Not_Author();
         }
-        return libraryBooks[_id];
+        return libraryBooks[_bookId].librarians;
     }
 
-    function getLibrarians(uint256 _id, uint256 _index) public view returns (address) {
-        return libraryBooks[_id].librarians[_index];
+    function getLibrarians(uint256 _bookId)
+        public
+        view
+        returns (address[] memory librarians)
+    {
+        return libraryBooks[_bookId].librarians;
+    }
+
+    function extendExpirationDate(uint256 _bookId, uint256 _addExpirationDays)
+        public
+        returns (bool success)
+    {
+        libraryBooks[_bookId].expirationDate += _addExpirationDays * 1 days;
+        for (uint256 i = 0; i < libraryBooks[_bookId].librarians.length; i++) {
+            if (libraryBooks[_bookId].librarians[i] == msg.sender) {
+                return true;
+            }
+            revert You_Are_Not_Librarian();
+        }
     }
 }
 
 //  0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+
 //  0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
+//  0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
 //  0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
