deaa9e8ddc997f4d425a6175714758ae1f67c72d
ijestanoff <jestanoff@icloud.com>
2025-01-28 22:57:42 +0200

    withdrawProfit & buyTicket

--- TicketsMarketplace/Marketplace.sol
+++ TicketsMarketplace/Marketplace.sol
@@ -6,6 +6,8 @@ import {Event} from "./Event.sol";
 error InvalidInput(string info);
 error AlreadyListed();
 error MustBeOrganizer();
+error WrongBuyingOption();
+error ProfitDistributionFailed();
 
 enum BuyingOption {
     FixedPrice,
@@ -20,7 +22,16 @@ struct EventData {
 
 contract Marketplace {
     uint256 constant MIN_SALE_PERIOD = 24 hours;
+    uint256 constant SALE_FEE = 0.1 ether;
+
+    address public immutable feeCollector;
+
     mapping(address => EventData) public events;
+    mapping (address => uint256) public profits;
+
+    constructor(address feeCollector){
+        feeCollector = feeCollector;
+    }
 
     function createEvent(
         string memory eventName,
@@ -56,6 +67,7 @@ contract Marketplace {
         BuyingOption saleType,
         uint256 salesEnds
     ) internal {
+        //TODO: Ensure External Event contract is compatible with IEvent
         if (salesEnds > block.timestamp + MIN_SALE_PERIOD) {
             revert InvalidInput("salesEnd is invalid");
         }
@@ -63,10 +75,37 @@ contract Marketplace {
             revert AlreadyListed();
         }
 
+        if (events[newEvent].ticketPrice < SALE_FEE) {
+            revert InvalidInput("ticketPrice >= sale fee");
+        }
+
         events[newEvent] = EventData({
             ticketPrice: ticketPrice,
             saleType: saleType,
             salesEnds: salesEnds
         });
+    }
+
+    function buyTicket(address event_) external payable{
+        if(events[event_].saleType != BuyingOption.FixedPrice) {
+            revert WrongBuyingOption();
+        }
+
+        if(msg.value != events[event_].ticketPrice) {
+            revert InvalidInput("wrong value");
+        }
+
+        profits[Event(event_).organizer()] += msg.value - SALE_FEE;
+        profits[feeCollector] += SALE_FEE;
+        Event(event_).safeMint(msg.sender);
+    }
+
+    function withdrawProfit(address to) external payable {
+        uint256 profit = profits[msg.sender];
+        profits[msg.sender] = 0;
+        (bool success, ) = to.call{value: profit}("");
+        if(!success) {
+            revert ProfitDistributionFailed();
+        }
     }
 }
